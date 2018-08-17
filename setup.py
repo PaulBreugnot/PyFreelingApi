61db729a0e998b175ae762b19a451634312ae132
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-17 10:23:01 -0500

    add package data

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='PyFreelingApi',
       author="Paul Breugnot",
       author_email='paul.breugnot@etu.emse.fr',
       packages=["pyFreelingApi", "pyFreelingApi.windows_api", "pyFreelingApi.linux_api"],
+      package_data={"pyFreelingApi.windows_api": "_pyfreeling.pyd", "pyFreelingApi.linux_api": "_pyfreeling.so"},
       description="Freeling Python APIs",
       long_description=open('README.md').read())
