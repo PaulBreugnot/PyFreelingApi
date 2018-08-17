1dde6e4dbc4863ffce4100da338b35a94a8d8724
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-17 10:29:29 -0500

    Correct package_data

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 
 setup(name='PyFreelingApi',
       version='0.1',
@@ -6,6 +7,6 @@ setup(name='PyFreelingApi',
       author="Paul Breugnot",
       author_email='paul.breugnot@etu.emse.fr',
       packages=["pyFreelingApi", "pyFreelingApi.windows_api", "pyFreelingApi.linux_api"],
-      package_data={"pyFreelingApi.windows_api": "_pyfreeling.pyd", "pyFreelingApi.linux_api": "_pyfreeling.so"},
+      package_data={"pyFreelingApi.windows_api": ["_pyfreeling.pyd"], "pyFreelingApi.linux_api": ["_pyfreeling.so"]},
       description="Freeling Python APIs",
       long_description=open('README.md').read())
