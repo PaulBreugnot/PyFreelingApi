3c2d08a5576f4f42513ce6db81520a028e9be077
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-17 10:00:07 -0500

    Correct imports

--- pyFreelingApi/__init__.py
+++ pyFreelingApi/__init__.py
@@ -2,7 +2,7 @@ import importlib
 import platform
 
 if platform.system() == "Windows":
-    freeling_api = importlib.import_module("windows_api.pyfreeling")
+    freeling_api = importlib.import_module("pyFreelingApi.windows_api.pyfreeling")
 
 elif platform.system() == "Linux":
-    freeling_api = importlib.import_module("windows_api.pyfreeling")
+    freeling_api = importlib.import_module("pyFreelingApi.linux_api.pyfreeling")
