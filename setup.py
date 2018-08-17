fcb93bcabe5ea175b27dbc715d170404ad3b2657
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-17 09:42:20 -0500

    pyFreelingApi package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='PyFreeling',
       version='0.1',
       url='https://github.com/PaulBreugnot/PyFreeling',
       author="Paul Breugnot",
       author_email='paul.breugnot@etu.emse.fr',
-      packages=[''],
+      packages=["pyFreelingApi"],
       description="Freeling Python APIs",
       long_description=open('README.md').read())
