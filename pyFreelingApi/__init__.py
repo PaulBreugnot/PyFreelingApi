ef691d7682070ad2ebc0c89da3802ec14f959490
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-21 12:14:33 -0500

    Refactor linux api path management

--- pyFreelingApi/__init__.py
+++ pyFreelingApi/__init__.py
@@ -1,5 +1,7 @@
 import importlib
 import platform
+import os
+LINUX_API_PATH = os.path.abspath(os.path.join(".", "linux_api", "_pyfreeling.so"))
 
 if platform.system() == "Windows":
     freeling_api = importlib.import_module("pyFreelingApi.windows_api.pyfreeling")
--- pyFreelingApi/linux_api/__init__.py
+++ pyFreelingApi/linux_api/__init__.py
@@ -1,4 +1,6 @@
+from pyFreelingApi import LINUX_API_PATH
+
 _pyfreeling_source = open("/usr/local/share/freeling/APIs/python3/_pyfreeling.so", mode="rb")
-_pyfreeling_target = open("linux_api/_pyfreeling.so", mode="w+b")
+_pyfreeling_target = open(LINUX_API_PATH, mode="w+b")
 _pyfreeling_target.write(_pyfreeling_source.read())
 
