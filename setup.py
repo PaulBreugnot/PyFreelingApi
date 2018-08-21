5e52d1f1ae8fed8e8270474c0881673b938ce625
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-21 11:40:27 -0500

    Update version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 import os
 
 setup(name='PyFreelingApi',
-      version='0.1',
+      version='0.1.1',
       url='https://github.com/PaulBreugnot/PyFreeling',
       author="Paul Breugnot",
       author_email='paul.breugnot@etu.emse.fr',
       packages=["pyFreelingApi", "pyFreelingApi.windows_api", "pyFreelingApi.linux_api"],
-      package_data={"pyFreelingApi.windows_api": ["_pyfreeling.pyd"], "pyFreelingApi.linux_api": ["_pyfreeling.so"]},
+      package_data={"pyFreelingApi.windows_api": ["_pyfreeling.pyd"]},
       description="Freeling Python APIs",
       long_description=open('README.md').read())
